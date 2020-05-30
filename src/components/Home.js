aadf0e149cc9b876f635451aa48b5ef2ed01a3ee
unknown <nhussaini@luc.edu>
2020-05-30 09:56:25 -0700

    mapped state to props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component{
-    state={
-        posts: []
-    }
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res =>{
-            this.setState({
-                posts: res.data.slice(0,10)
-            })
-        })
-    }
+
     render(){
-       // const { posts }= this.state;
-       const posts= this.state.posts
+       // const { posts }= this.props;
+       const posts= this.props.posts;
         const postList = posts.length ? (
             posts.map(post =>{
                 return (
@@ -41,7 +31,12 @@ class Home extends Component{
                 {postList}
             </div>
         );
-    }
-    
+    }   
 }
-export default Home;
+
+const mapStateToProps =(state)=>{
+    return {
+        posts: state.posts
+    }
+}
+export default connect(mapStateToProps)(Home);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 const initState ={
-    posts:[]
+    posts:[
+        {id: '1', title: 'Squirtle Laid an egg', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '},
+        {id: '2', title:'Charmander laid on egg', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '},
+        {id: '3', title: 'A helix fossil was found', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '},
+    ]
 }
 const rootReducer = (state=initState, action) =>{
-
+    return state;
 }
 
 export default rootReducer;
